fac1eed4c8921e79465ca9de0eca8a14ced0b286
ch4tterb0x <ch4tterb0x@users.noreply.github.com>
2021-01-27 22:07:35 +0100

    General improvements Simplified initiation of standard values when script is not run in a widget. Widget no longer fails when parameters are not given for exactly 5 contacts. Widget is now able to show up to 5 contacts.

--- threema_contacts.js
+++ threema_contacts.js
@@ -10,52 +10,28 @@
 //The Parameter has to put like this: name,thID,name,thID,name,thID,name,thID,name,thID
 //Example: ECHOECHO,ECHOECHO,Threema.ch,*THREEMA,Support,*SUPPORT,MyData,*MY3DATA,Reg-Bawü,*REGBAWU
 
-let name =[];
-let thID =[];
-name[0] = "ECHO";
-thID[1] = "ECHOECHO";
-name[2] = "Aniken";
-thID[3] = "*THREEMA";
-name[4] = "Eric";
-thID[5] = "*SUPPORT";
-name[6] = "MyData";
-thID[7] = "*MY3DATA";
-name[8] = "Nico";
-thID[9] = "*REGBAWU";
+/** @type {{id:string, name:string}[]} */
+const contacts = []
 
+/** @type {string[]} */
+let params
 if (!config.runsInWidget) {
-    
-    name[0];
-    thID[1];
-    name[2];
-    thID[3];
-    name[4];
-    thID[5];
-    name[6];
-    thID[7];
-    name[8];
-    thID[9];
-    
+  const parameters = 'ECHO,ECHOECHO,Aniken,*THREEMA,Eric,*SUPPORT,MyData,*MY3DATA,Nico,*REGBAWU'
+  params = parameters.split(',')
 } else {
+  params = args.widgetParameter?.split(',') ?? []
+}
 
-const params = args.widgetParameter ? args.widgetParameter.split(",") : undefined
+let i = 0
+while (i + 1 < params.length && i < 2 * 5) { // adds up to 5 contacts
+  const name = params[i]
+  const id = i + 1 < params.length ? params[i + 1] : name
+  contacts.push({ id: id, name: name })
+  i += 2
+}
 
-    name[0] = params[0]
-    thID[1] = params[1] //4 Parameter für das Widget können hinterlegt werden. 
-    name[2] = params[2]
-    thID[3] = params[3] //Parameter für das Widget
-    name[4] = params[4]
-    thID[5] = params[5] //Parameter für das Widget
-    name[6] = params[6]
-    thID[7] = params[7] //Parameter für das Widget
-    name[8] = params[8]
-    thID[9] = params[9] 
- 
-  }
 // URL Link
-let url = [];
-url[0] = "https://threema.id/";
-url[1] = "?text=";
+const url = ['https://threema.id/', '?text=']
 
 // Images for the Contacts; Copyright 2020 by "netzpython27"
 let contactURL = "https://raw.githubusercontent.com/netzpython27/contact_threema_widget/main/messagephone.png";
@@ -76,45 +52,14 @@ widget.addSpacer(4)
 
 let row = widget.addStack()
 
-row.addSpacer(10)//Helps to centralice the image
+row.addSpacer(10) // Helps to center align the image
 //Add Contacts
-addContact(
-  contactImg,
-  name[0], //and his name.
-    url[0] + thID[1] + url[1], //URL Shortcut to the contact
-  row)
-
-row.addSpacer(10)//Helps to centralice the image
-
-addContact(
-  contactImg,
-  name[2],
-  url[0] + thID[3] + url[1],
-  row)
-row.addSpacer(10)//Helps to centralice the image
-
-addContact(
-  contactImg,
-  name[4],
-  url[0] + thID[5] + url[1],
-  row)
-
-  row.addSpacer(10)//Helps to centralice the image
-
-addContact(
-    contactImg,
-    name[6],
-    url[0] + thID[7] + url[1],
-    row)
-
- row.addSpacer(10)//Helps to centralice the image
-
-addContact(
-    contactImg,
-    name[8],
-    url[0] + thID[9] + url[1],
-    row)
- row.addSpacer(10)//Helps to centralice the image
+contacts.forEach(contact => {
+  // Add contact
+  const contact_url = url[0] + contact.id + url[1] //URL Shortcut to the contact
+  addContact(contactImg, contact.name, contact_url, row)
+  row.addSpacer(10) // Helps to center align the image
+})
 
 widget.presentMedium()
 
